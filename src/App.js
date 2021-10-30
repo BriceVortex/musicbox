df3e343c7ee0030fd0b89fc1846f1964c66218e8
BriceVortex <brice.zuninomourlon@gmail.com>
2021-10-30 14:52:22 +0200

    Editing index App JSON packages and README files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+// Import Styles
+import './styles/app.scss';
+// Adding Components
+import Player from './components/Player';
+import Song from './components/Song';
+// Import Music Data
+import data from './data'
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[4]);
   return (
     <div className="App">
-      <h1>Music Box</h1>
+      <Song currentSong={currentSong} />
+      <Player />
     </div>
   );
 }
